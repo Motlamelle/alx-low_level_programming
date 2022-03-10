1abb2212b25254742ba6f2f0bca8418320851c73
Motlamelle <lebomotlamelle@gmail.com>
2022-03-10 15:17:58 +0200

    print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print text followed by a new line using puts function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
