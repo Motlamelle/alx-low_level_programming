80126a6d4aa882197119aab83178c4b5a3afb1b6
Motlamelle <lebomotlamelle@gmail.com>
2022-03-10 21:34:16 +0200

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main -Print text followed by a new line using printf function
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
