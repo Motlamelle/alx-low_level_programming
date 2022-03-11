8e1f535eff94ffa937706b196a61773d32202cf1
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 18:24:56 +0200

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+		putchar(c);
+	for (c = 'a' ; c <= 'f' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
