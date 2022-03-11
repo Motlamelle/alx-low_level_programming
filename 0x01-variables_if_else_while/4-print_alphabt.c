01f093b2b4f8e8151050903cf3b33b759ab545d1
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 15:40:29 +0200

    print alphabet expect e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		if ((c != 'e') && (c != 'q'))
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
