253330e72ef7d9b7e620dffd5e274d55e7d87a95
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 22:05:25 +0200

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of three digits
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (n = 48 ; n < 58 ; n++)
+	{
+		for (m = 49 ; m < 58 ; m++)
+		{
+			for (l = 50 ; l < 58 ; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+
+					}
+
+				}
+			}
+
+		}
+
+
+	}
+	putchar('\n');
+	return (0);
+}
