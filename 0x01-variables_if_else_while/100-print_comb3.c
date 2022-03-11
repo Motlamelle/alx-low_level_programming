06e41823d974fa64ed97ec63dfc2f79af8634623
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 21:04:06 +0200

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,22 @@ int main(void)
 
 	for (c = 0 ; c < 9 ; c++)
 	{
-		for (d = 1 ; d <= 9 ; d++)
-		{	putchar(c % 10 + '0');
+		d = c + 1;
+		for ( ; d <= 9 ; d++)
+		{
+			putchar(c % 10 + '0');
 			putchar(d % 10 + '0');
 
-			if (d != 9)
-			{	putchar(',');
+			if (c < 8 || d < 9)
+			{
+				putchar(',');
 				putchar(' ');
+
 			}
 		}
+
+
 	}
-	putchar('\n');
+
 	return (0);
 }
