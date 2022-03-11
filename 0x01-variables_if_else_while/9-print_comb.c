58342d4608101fcf4fc42b05d1cf288f93e2ec8b
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 18:51:33 +0200

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{	putchar(c);
+		if (c < '9')
+		{	putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
