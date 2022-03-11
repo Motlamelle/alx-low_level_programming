b680b0d78fe9114d9df0f44e88d43be613d788e6
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 18:04:23 +0200

    print reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c>= 'a' ; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
