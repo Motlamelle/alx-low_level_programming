23ce740b2b74c7f119b70c6d6203dd8aa4c99cae
Motlamelle <lebomotlamelle@gmail.com>
2022-03-11 15:21:28 +0200

    print lower case and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	for (c = 'A' ; c <= 'Z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
