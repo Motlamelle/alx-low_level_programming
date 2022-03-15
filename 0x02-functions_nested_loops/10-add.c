d4f2c2390922da908fb074546be981c9b73b67e4
Motlamelle <lebomotlamelle@gmail.com>
2022-03-15 22:58:13 +0200

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add intergers a and b
+ * @a: first argument
+ * @b: second argument
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
