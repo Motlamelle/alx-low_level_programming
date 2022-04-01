015bd36acf8a45f74f473f0cee7600d34d493453
Motlamelle <lebomotlamelle@gmail.com>
2022-04-01 21:52:02 +0200

    create static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - check if the character is lowerCase
+ * @c: the character to be checked
+ * Return: 1 lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * greater than 0 otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
