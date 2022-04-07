a7e6d34756c99d2acc95dee71a83bf70836ec687
Motlamelle <lebomotlamelle@gmail.com>
2022-04-07 19:35:58 +0200

    malloc and exit (98)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory in bytes to allocate
+ * Return: pointer to the address of the allocated memory (SUCCESS),
+ * Exit status 98 (FAILURE)
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
