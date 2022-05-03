fd10bfad59e00d98ce9ac1c913fe859ea649dd1a
Motlamelle <lebomotlamelle@gmail.com>
2022-05-03 19:34:53 +0200

    remove extra returns

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,21 +19,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters + 1);
+
+	buf = malloc(sizeof(char) * letters);
 
 	if (buf == NULL)
 		return (0);
 
 	rd = read(fd, buf, letters);
-	if (rd == -1)
-		return (0);
 
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-
-	if (wr == -1)
-		return (0);
+	wr = write(STDOUT_FILENO, buf, rd);
 
 	close(fd);
 	free(buf);
