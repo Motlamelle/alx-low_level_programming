333d374a3477e8890a24c7df26704bda18d4ac9d
Motlamelle <lebomotlamelle@gmail.com>
2022-05-03 19:55:24 +0200

    fix checker

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,16 +13,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-
 	if (text_content == NULL)
 		text_content = "";
+
+	while (text_content[i])
+		i++;
 
 	write(file, text_content, i);
 
