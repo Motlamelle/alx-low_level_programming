c401c807b59b7e31464909f4ec7ab8c704f4ba1f
Motlamelle <lebomotlamelle@gmail.com>
2022-05-04 14:16:05 +0200

    add updated files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -11,5 +11,15 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void validate_elf(unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
 
 #endif
